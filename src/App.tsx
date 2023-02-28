69fcf86fbb97f4b988ddb3b8fb17add70a4bab24
Refuse94 <simko.tomas94@gmail.com>
2023-02-28 17:13:01 +0100

    create functions folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Default } from "./components/default";
 import { NewBlog } from "./components/form";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useFetch } from "./functions/useFetch";
 
 export const App: React.FC = () => {
   return (
